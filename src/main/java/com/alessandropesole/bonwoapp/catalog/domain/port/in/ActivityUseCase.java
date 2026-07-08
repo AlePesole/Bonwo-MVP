fee2f8003a990f7e10ef6cb12737b0da85dc4310
AlessandroPesole <alespesole@gmail.com>
2026-07-08 17:51:51 +0200

    feat: added activity useCase interface

---
+++ src/main/java/com/alessandropesole/bonwoapp/catalog/domain/port/in/ActivityUseCase.java
@@ -0,0 +1,16 @@
+package com.alessandropesole.bonwoapp.catalog.domain.port.in;
+
+import com.alessandropesole.bonwoapp.catalog.application.dto.ActivityRequest;
+import com.alessandropesole.bonwoapp.catalog.application.dto.ActivityResponse;
+
+import java.util.List;
+
+public interface ActivityUseCase {
+    List<ActivityResponse> listAll();
+
+    ActivityResponse create(ActivityRequest request);
+
+    ActivityResponse update(Long id, ActivityRequest request);
+
+    void delete(Long id);
+}
