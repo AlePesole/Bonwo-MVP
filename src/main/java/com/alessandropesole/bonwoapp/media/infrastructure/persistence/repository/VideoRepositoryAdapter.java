f89bf83f109037cbae3d688a104b3a77b0f5860e
AlessandroPesole <alespesole@gmail.com>
2026-07-08 17:20:48 +0200

    feat: added video repository implementation

---
+++ src/main/java/com/alessandropesole/bonwoapp/media/infrastructure/persistence/repository/VideoRepositoryAdapter.java
@@ -0,0 +1,42 @@
+package com.alessandropesole.bonwoapp.media.infrastructure.persistence.repository;
+
+import com.alessandropesole.bonwoapp.media.domain.model.Video;
+import com.alessandropesole.bonwoapp.media.domain.port.out.VideoRepository;
+import com.alessandropesole.bonwoapp.media.infrastructure.persistence.mapper.MediaPersistenceMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class VideoRepositoryAdapter implements VideoRepository {
+    private final VideoJpaRepository jpa;
+
+    @Override
+    public Video save(Video v) {
+        return MediaPersistenceMapper.toDomain(jpa.save(MediaPersistenceMapper.toEntity(v)));
+    }
+
+    @Override
+    public Optional<Video> findById(Long id) {
+        return jpa.findById(id).map(MediaPersistenceMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Video> findByUploadToken(String t) {
+        return jpa.findByUploadToken(t).map(MediaPersistenceMapper::toDomain);
+    }
+
+    @Override
+    public List<Video> findAllExpiredPending() {
+        return jpa.findAllExpiredPending(Instant.now()).stream().map(MediaPersistenceMapper::toDomain).toList();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        jpa.deleteById(id);
+    }
+}
