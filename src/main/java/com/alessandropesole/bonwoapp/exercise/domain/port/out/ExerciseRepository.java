f284bcc65ed2a51a4e1d440bc3306acc6acbe2df
AlessandroPesole <alespesole@gmail.com>
2026-07-20 21:52:04 +0200

    feat: exercise repository interface

---
+++ src/main/java/com/alessandropesole/bonwoapp/exercise/domain/port/out/ExerciseRepository.java
@@ -0,0 +1,19 @@
+package com.alessandropesole.bonwoapp.exercise.domain.port.out;
+
+import com.alessandropesole.bonwoapp.exercise.domain.model.Exercise;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface ExerciseRepository {
+    Exercise save(Exercise exercise);
+
+    Optional<Exercise> findById(Long id);
+
+    Page<Exercise> findByOwner(Long ownerId, Set<Long> muscleSubGroupIds, Set<Long> equipmentIds,
+                               Set<Long> activityIds, Set<Long> trainingGoalIds, Pageable pageable);
+
+    void deleteById(Long id);
+}
