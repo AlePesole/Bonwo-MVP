28662d7a2064b1322afbac1223cdf1ab83a21a1a
AlessandroPesole <alespesole@gmail.com>
2026-08-31 22:17:16 +0200

    feat: added new cleanup orphan images

--- src/main/java/com/alessandropesole/bonwoapp/BonwoappApplication.java
+++ src/main/java/com/alessandropesole/bonwoapp/BonwoappApplication.java
@@ -2,8 +2,10 @@ package com.alessandropesole.bonwoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BonwoappApplication {
 
 	public static void main(String[] args) {
